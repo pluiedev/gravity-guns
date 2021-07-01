05004defb3a724aebdc813fe608f51233580bce9
leoc200 <22406910+LeoCTH@users.noreply.github.com>
2021-07-02 02:33:50 +0800

    new: animations

--- src/main/kotlin/com/leocth/gravityguns/item/GravityGunItem.kt
+++ src/main/kotlin/com/leocth/gravityguns/item/GravityGunItem.kt
@@ -3,26 +3,41 @@ package com.leocth.gravityguns.item
 import com.leocth.gravityguns.GravityGuns
 import com.leocth.gravityguns.physics.GrabUtil
 import com.leocth.gravityguns.physics.GrabbingManager
+import com.leocth.gravityguns.util.ext.setAnimation
 import com.leocth.gravityguns.util.ext.toBullet
 import dev.lazurite.rayon.core.impl.bullet.thread.PhysicsThread
+import net.fabricmc.fabric.api.networking.v1.PlayerLookup
 import net.minecraft.entity.LivingEntity
 import net.minecraft.entity.player.PlayerEntity
 import net.minecraft.item.Item
 import net.minecraft.item.ItemStack
 import net.minecraft.server.network.ServerPlayerEntity
+import net.minecraft.server.world.ServerWorld
 import net.minecraft.util.Hand
 import net.minecraft.util.TypedActionResult
 import net.minecraft.world.World
+import software.bernie.geckolib3.core.AnimationState
 import software.bernie.geckolib3.core.IAnimatable
 import software.bernie.geckolib3.core.PlayState
 import software.bernie.geckolib3.core.controller.AnimationController
 import software.bernie.geckolib3.core.manager.AnimationData
 import software.bernie.geckolib3.core.manager.AnimationFactory
+import software.bernie.geckolib3.network.GeckoLibNetwork
+import software.bernie.geckolib3.network.ISyncable
+import software.bernie.geckolib3.util.GeckoLibUtil
 
-class GravityGunItem(settings: Settings) : Item(settings), IAnimatable {
+class GravityGunItem(settings: Settings) : Item(settings), IAnimatable, ISyncable {
     private val factory = AnimationFactory(this)
 
+    init {
+        GeckoLibNetwork.registerSyncable(this)
+    }
+
     companion object {
+        private const val CONTROLLER_ID = "controller"
+        private const val EXTEND = 0
+        private const val RETRACT = 1
+
         var ItemStack.power: Double
             get() = tag?.getDouble("power") ?: 0.0
             set(value) { orCreateTag.putDouble("power", value) }
@@ -45,14 +60,15 @@ class GravityGunItem(settings: Settings) : Item(settings), IAnimatable {
                 }
 
                 grabbingManager.tryUngrab(user, 5.0f)
+                syncAnimation(user, world, stack, RETRACT)
             }
         }
     }
 
     override fun use(world: World, user: PlayerEntity, hand: Hand): TypedActionResult<ItemStack> {
-        if (!world.isClient) {
-            val stack = user.getStackInHand(hand)
+        val stack = user.getStackInHand(hand)
 
+        if (!world.isClient) {
             val grabbingManager = GrabbingManager.SERVER
 
             if (!grabbingManager.isPlayerGrabbing(user)) {
@@ -65,6 +81,8 @@ class GravityGunItem(settings: Settings) : Item(settings), IAnimatable {
 
                 grabbingManager.tryGrab(user, thingToGrab)
                 user.setCurrentHand(hand)
+                syncAnimation(user, world, stack, EXTEND)
+
                 return TypedActionResult.consume(stack)
             }
         }
@@ -72,10 +90,7 @@ class GravityGunItem(settings: Settings) : Item(settings), IAnimatable {
     }
 
     override fun registerControllers(data: AnimationData) {
-        val controller = AnimationController(this, "controller", 5f) { event ->
-            //event.controller.setAnimation {
-                //it.addAnimation("test")
-            //}
+        val controller = AnimationController(this, CONTROLLER_ID, 0f) {
             PlayState.CONTINUE
         }
 
@@ -83,4 +98,29 @@ class GravityGunItem(settings: Settings) : Item(settings), IAnimatable {
     }
 
     override fun getFactory(): AnimationFactory = factory
+
+    fun syncAnimation(user: PlayerEntity, world: World, stack: ItemStack, anim: Int) {
+        if (world !is ServerWorld) throw IllegalStateException("syncAnimation may *only* be called on the logical server")
+
+        val id = GeckoLibUtil.guaranteeIDForStack(stack, world)
+        GeckoLibNetwork.syncAnimation(user, this, id, anim)
+        for (otherPlayer in PlayerLookup.tracking(user)) {
+            GeckoLibNetwork.syncAnimation(otherPlayer, this, id, anim)
+        }
+    }
+
+    override fun onAnimationSync(id: Int, state: Int) {
+        val controller = GeckoLibUtil.getControllerForID(factory, id, CONTROLLER_ID)
+        controller.markNeedsReload()
+        controller.setAnimation {
+            when (state) {
+                EXTEND -> it
+                    .addAnimation("animation.gravity_gun.extend")
+                    .addAnimation("animation.gravity_gun.open")
+                RETRACT -> it
+                    .addAnimation("animation.gravity_gun.retract")
+                    .addAnimation("animation.gravity_gun.closed")
+            }
+        }
+    }
 }
