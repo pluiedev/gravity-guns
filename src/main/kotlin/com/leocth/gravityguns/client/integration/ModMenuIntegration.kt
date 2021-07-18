ec435216aab8f54f4f958817eee2141122b8de65
leoc200 <22406910+LeoCTH@users.noreply.github.com>
2021-07-18 13:17:31 +0800

    adjust default maximum power since 5 is apparently big enough to lag my computer

--- src/main/kotlin/com/leocth/gravityguns/client/integration/ModMenuIntegration.kt
+++ src/main/kotlin/com/leocth/gravityguns/client/integration/ModMenuIntegration.kt
@@ -1,6 +1,7 @@
 package com.leocth.gravityguns.client.integration
 
 import com.leocth.gravityguns.GravityGuns
+import com.leocth.gravityguns.config.GravityGunsConfig
 import com.terraformersmc.modmenu.api.ConfigScreenFactory
 import com.terraformersmc.modmenu.api.ModMenuApi
 import me.shedaniel.clothconfiglite.api.ConfigScreen
@@ -11,12 +12,13 @@ import kotlin.reflect.KMutableProperty0
 object ModMenuIntegration: ModMenuApi {
     override fun getModConfigScreenFactory() = ConfigScreenFactory {
         val config = GravityGuns.CONFIG
+        val def = GravityGunsConfig()
 
         ConfigScreen.create(TranslatableText("screen.gravityguns.config"), it).apply {
-            addProp("screen.gravityguns.config.maximumPowerLevel", config::maximumPowerLevel, 5.0)
-            addProp("screen.gravityguns.config.launchInitialVelocityMultiplier", config::launchInitialVelocityMultiplier, 20.0)
-            addProp("screen.gravityguns.config.entityReachDistance", config::entityReachDistance, 7.0)
-            addProp("screen.gravityguns.config.blockReachDistance", config::blockReachDistance, 8.0)
+            addProp("screen.gravityguns.config.maximumPowerLevel", config::maximumPowerLevel, def.maximumPowerLevel)
+            addProp("screen.gravityguns.config.launchInitialVelocityMultiplier", config::launchInitialVelocityMultiplier, def.launchInitialVelocityMultiplier)
+            addProp("screen.gravityguns.config.entityReachDistance", config::entityReachDistance, def.entityReachDistance)
+            addProp("screen.gravityguns.config.blockReachDistance", config::blockReachDistance, def.blockReachDistance)
         }.get()
     }
 
