d0002e8f460dcea1b70f8a67207cf0b418092265
leoc200 <22406910+LeoCTH@users.noreply.github.com>
2021-07-18 15:44:27 +0800

    clean up NBT stuff

--- src/main/kotlin/com/leocth/gravityguns/util/ext/NbtExtensions.kt
+++ src/main/kotlin/com/leocth/gravityguns/util/ext/NbtExtensions.kt
@@ -1,68 +1,13 @@
 @file:Suppress("NOTHING_TO_INLINE")
 package com.leocth.gravityguns.util.ext
 
-import net.fabricmc.fabric.api.util.NbtType
-import net.minecraft.block.BlockState
-import net.minecraft.item.ItemStack
 import net.minecraft.nbt.NbtCompound
-import net.minecraft.nbt.NbtElement
-import net.minecraft.nbt.NbtHelper
-import net.minecraft.nbt.NbtIntArray
-import net.minecraft.util.Identifier
+import net.minecraft.util.math.BlockPos
 
-inline fun NbtCompound.getCompound(key: String, action: (NbtCompound) -> Unit): NbtCompound {
-    val compound = getCompound(key)
-    action(compound)
-    return compound
-}
-
-inline fun NbtCompound.putCompound(key: String, init: (NbtCompound) -> Unit) {
-    put(key, NbtCompound().apply(init))
-}
-
-inline fun NbtCompound.getIdentifier(key: String): Identifier
-    = Identifier(getString(key))
-
-inline fun NbtCompound.putIdentifier(key: String, id: Identifier) {
-    putString(key, id.toString())
-}
-
-inline fun NbtCompound.getItemStack(key: String): ItemStack
-    = ItemStack.fromNbt(getCompound(key))
-
-inline fun NbtCompound.putItemStack(key: String, stack: ItemStack)
-    = putCompound(key, stack::writeNbt)
-
-inline fun NbtCompound.getBlockState(key: String)
-    = NbtHelper.toBlockState(getCompound(key))
-
-inline fun NbtCompound.putBlockState(key: String, state: BlockState) {
-    put(key, NbtHelper.fromBlockState(state))
-}
-
-inline fun NbtCompound.find(key: String, type: Int, ifFound: (NbtElement) -> Unit = {}): NbtElement? {
-    val element = get(key)
-    if (element != null && element.type == type.toByte())
-        ifFound(element)
-    return element
-}
-
-inline fun NbtCompound.findCompound(key: String, ifFound: (NbtCompound) -> Unit = {}): NbtCompound? {
-    val element = get(key)
-    if (element != null && element.type == NbtType.COMPOUND.toByte()) {
-        val compound = element as NbtCompound
-        ifFound(element)
-        return element
+inline fun NbtCompound.getBlockPos(key: String): BlockPos
+    = getLong(key).let {
+        BlockPos.fromLong(it)
     }
-    return null
-}
 
-inline fun NbtCompound.findIntArray(key: String, ifFound: (IntArray) -> Unit = {}): NbtIntArray? {
-    val element = get(key)
-    if (element != null && element.type == NbtType.INT_ARRAY.toByte()) {
-        val e = element as NbtIntArray
-        ifFound(e.intArray)
-        return e
-    }
-    return null
-}
+inline fun NbtCompound.putBlockPos(key: String, pos: BlockPos)
+    = putLong(key, pos.asLong())
