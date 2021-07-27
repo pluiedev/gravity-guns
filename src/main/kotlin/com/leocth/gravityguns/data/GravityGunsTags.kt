8f2e0d4aa97966a87a950a5b054fa511a39f660b
leoc200 <22406910+LeoCTH@users.noreply.github.com>
2021-07-27 19:35:16 +0800

    added allow lists to *only* allow certain blocks and entities to be grabbed

--- src/main/kotlin/com/leocth/gravityguns/data/GravityGunsTags.kt
+++ src/main/kotlin/com/leocth/gravityguns/data/GravityGunsTags.kt
@@ -10,5 +10,21 @@ object GravityGunsTags {
     val IMMOBILE_ENTITIES: Tag<EntityType<*>> = TagRegistry.entityType(GravityGuns.id("immobile"))
     val IMMOBILE_BLOCKS: Tag<Block> = TagRegistry.block(GravityGuns.id("immobile"))
 
+    // Basically these act as 'allow lists', and overrides the 'deny lists' above whenever they are present.
+    val MOBILE_ENTITIES: Tag<EntityType<*>> = TagRegistry.entityType(GravityGuns.id("mobile"))
+    val MOBILE_BLOCKS: Tag<Block> = TagRegistry.block(GravityGuns.id("mobile"))
+
     fun register() { /* NO-OP */ }
+
+    fun isImmobile(block: Block) =
+        if (MOBILE_BLOCKS.values().isNotEmpty())
+            block !in MOBILE_BLOCKS
+        else
+            block in IMMOBILE_BLOCKS
+
+    fun isImmobile(entityType: EntityType<*>) =
+        if (MOBILE_ENTITIES.values().isNotEmpty())
+            entityType !in MOBILE_ENTITIES
+        else
+            entityType in IMMOBILE_ENTITIES
 }
