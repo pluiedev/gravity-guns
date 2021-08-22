d39dd9da33e97b7f7283fcbb7d0bc3d23266f1cd
leoc200 <22406910+leocth@users.noreply.github.com>
2021-08-22 10:09:13 +0800

    update to 1.17.1, use version catalogs and cleanup

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -4,8 +4,21 @@ pluginManagement {
         maven("https://maven.fabricmc.net/") {
             name = "Fabric"
         }
+        maven("https://server.bbkr.space/artifactory/libs-release/") {
+            name = "Cotton"
+        }
         gradlePluginPortal()
     }
 }
 
 rootProject.name = "gravity-guns"
+
+enableFeaturePreview("VERSION_CATALOGS")
+
+dependencyResolutionManagement {
+    versionCatalogs {
+        create("libs") {
+            from(files("libs.versions.toml"))
+        }
+    }
+}
